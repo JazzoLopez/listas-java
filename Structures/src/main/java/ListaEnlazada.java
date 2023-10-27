189be3dd0f34ca4034aa1efc2f2e330c5f01b482
JazzieL Rodríguez López <jazzielrodriguezlopez@gmail.com>
2023-10-26 21:05:06 -0600

    Practicando con un control de estudiantes

--- Structures/src/main/java/ListaEnlazada.java
+++ Structures/src/main/java/ListaEnlazada.java
@@ -7,7 +7,7 @@ public class ListaEnlazada {
         int e, op;
         do {
             op = Integer.parseInt(JOptionPane.showInputDialog(
-                    "Selecciona:\n1. Agregar al inicio\n2. Agregar al final\n3. Eliminar al inicio \n 4. Mostrar \n4. Salir"));
+                            "Selecciona:\n1. Agregar al inicio\n2. Agregar al final\n3. Eliminar al inicio \n4. Eliminar al final \n5. Eliminar nodo especifico \n6. Mostrar lista \n7. Mostrar elemento especifico \n 8.Salir"));
 
             switch (op) {
                 case 1:
@@ -19,17 +19,34 @@ public class ListaEnlazada {
                     lsl.insertarFinal(e);
                     break;
                 case 3:
-                    lsl.eliminarInicioSiCoincide(); // Elimina automáticamente si el valor del elemento coincide
+                    lsl.eliminarInicio(); // Elimina automáticamente si el valor del elemento coincide
                     break;
                 case 4:
-                    lsl.inprimirLista();
+                    lsl.eliminarFinal();
                     break;
                 case 5:
-                    JOptionPane.showMessageDialog(null, "Nos vemos pronto", "Adiós", 2);
+                    lsl.imprimirLista();
+                    System.out.println("Escribe el numero que deseas eliminar");
+                    e = Integer.parseInt(JOptionPane.showInputDialog("busca y escribe cual eliminaras")); 
+                    lsl.eliminarEspecifico(e);
                     break;
+                case 6:
+                   lsl.imprimirLista();
+
+                break;
+                case 7:
+                lsl.imprimirLista();
+                System.out.println("Comprueba si lo que ingresas existe jeje :)");
+                  e = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el dato"));
+                    lsl.encontrarElemento(e);
+                    
+                break;
+                case 8:
+                    JOptionPane.showMessageDialog(null,"Nos vemos pronto","Adios",2);
+                break;
                 default:
-                    break;
+                break;
             }
-        } while (op != 5);
+        } while (op != 8);
     }
 }
